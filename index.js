712247ddb1a9740c53c3ef04b6c4ee03ee927687
YehosuaEs <yehosuaes@gmail.com>
2022-08-04 18:33:20 +0200

    adding description

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function Calculate() {
-
+    /* Getting Values from inputs */
     let height = document.getElementById("height").value;
     let weight = document.getElementById("weight").value;
-    let printBmi = document.getElementById("result");
-
+    /* Calculation */
     let bmi = parseInt(weight) / Math.pow(parseInt(height), 2);
     let resultFixed = (bmi * 10000).toFixed(2);
-
+    /* Giving the values after calculate */
+    let printBmi = document.getElementById("result");
     let notNum = printBmi.innerHTML = `<div> Please enter a correct numer </div>`;
     let printResult = printBmi.innerHTML = `<div>${resultFixed}</div>`;
 
-    isNaN(height) || height === "" || isNaN(weight) || weight === "" ? notNum : printResult;
+    return isNaN(height) || height === "" || isNaN(weight) || weight === "" ? notNum : printResult;
 
 }
 
